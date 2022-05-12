51d1481ee945fbd60a266ffcf04036e8f075bf89
Mohamed Hossam <m.hossameldine@gmail.com>
2022-05-12 19:24:26 +0200

    feat: :sparkles: add basic structure

--- src/App.js
+++ src/App.js
@@ -1,5 +1,27 @@
+import { Switch, Route } from 'react-router-dom';
+import Home from './pages/home/Home';
+import Login from './pages/login/Login';
+import Signup from './pages/signup/Signup';
+import Navbar from './components/Navbar';
+
 function App() {
-  return <div className='App'></div>;
+  return (
+    <div className='App'>
+      <Navbar />
+
+      <Switch>
+        <Route path='/' exact>
+          <Home />
+        </Route>
+        <Route path='/login'>
+          <Login />
+        </Route>
+        <Route path='/signup'>
+          <Signup />
+        </Route>
+      </Switch>
+    </div>
+  );
 }
 
 export default App;
