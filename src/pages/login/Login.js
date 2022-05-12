75207c6821ea28225e393fc6924cc88120a0204d
Mohamed Hossam <m.hossameldine@gmail.com>
2022-05-12 19:54:38 +0200

    feat(pages): :sparkles: add login page structure

--- src/pages/login/Login.js
+++ src/pages/login/Login.js
@@ -1,5 +1,36 @@
-import React from 'react';
+import { useState } from 'react';
 
-export default function login() {
-  return <div>login</div>;
+// styels
+import styles from './Login.module.css';
+
+export default function Login() {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log(email, password);
+  };
+
+  return (
+    <form onSubmit={handleSubmit} className={styles['login-form']}>
+      <label>
+        <span>Email:</span>
+        <input
+          type='email'
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+        />
+      </label>
+      <label>
+        <span>Password:</span>
+        <input
+          type='password'
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+        />
+      </label>
+      <button className='btn'>Login</button>
+    </form>
+  );
 }
