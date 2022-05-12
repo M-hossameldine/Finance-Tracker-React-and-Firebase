01118dde31475c4069b35cab2230062a0959fe50
Mohamed Hossam <m.hossameldine@gmail.com>
2022-05-12 20:11:03 +0200

    feat(pages): :sparkles: add signup basic structure

--- src/pages/signup/Signup.js
+++ src/pages/signup/Signup.js
@@ -1,5 +1,44 @@
-import React from 'react';
+import React, { useState } from 'react';
 
-export default function signup() {
-  return <div>signup</div>;
+// styles
+import styles from './Signup.module.css';
+
+export default function Signup() {
+  const [name, setName] = useState('');
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    console.log(name, email, password);
+  };
+  return (
+    <form onSubmit={handleSubmit} className={styles['signup-form']}>
+      <label>
+        <span>Name:</span>
+        <input
+          type='text'
+          value={name}
+          onChange={(e) => setName(e.target.value)}
+        />
+      </label>
+      <label>
+        <span>Email:</span>
+        <input
+          type='email'
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+        />
+      </label>
+      <label>
+        <span>Password:</span>
+        <input
+          type='password'
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+        />
+      </label>
+      <button className='btn'>Signup</button>
+    </form>
+  );
 }
