753fe9ab1ddc34e239a49946290c7609bd8b706c
Mohamed Hossam <m.hossameldine@gmail.com>
2022-05-14 22:23:07 +0200

    feat:  add logout hook and logout action

---
+++ src/hooks/useLogout.js
@@ -0,0 +1,33 @@
+import { useState } from 'react';
+import { projectAuth } from '../firebase/config';
+import { useAuthContext } from './useAuthContext';
+
+export const useLogout = () => {
+  const [error, setError] = useState(null);
+  const [isPending, setIsPending] = useState(false);
+  const { dispatch } = useAuthContext();
+
+  const logout = async () => {
+    setError(null);
+    setIsPending(true);
+
+    // sign the use out
+    try {
+      // send logout request
+      const res = await projectAuth.signOut();
+
+      /* successful response */
+      // dispatch logout action
+      dispatch({ type: 'LOGOUT' });
+
+      setIsPending(false);
+      setError(null);
+    } catch (err) {
+      console.log(err.message);
+      setError(err.message);
+      setIsPending(false);
+    }
+  };
+
+  return { error, isPending, logout };
+};
