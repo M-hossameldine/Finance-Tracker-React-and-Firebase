94bdb022cd6ce31c39d0f101ee8fc5561b572a08
Mohamed Hossam <m.hossameldine@gmail.com>
2022-05-14 17:56:18 +0200

    feat(hooks): :sparkles: add signup reusable hook

---
+++ src/hooks/useSignup.js
@@ -0,0 +1,39 @@
+import { useState } from 'react';
+import { projectAuth } from '../firebase/config';
+
+export const useSignup = () => {
+  const [error, setError] = useState(null);
+  const [isPending, setIsPending] = useState(false);
+
+  const signup = async (email, password, displayName) => {
+    setError(null);
+    setIsPending(true);
+
+    try {
+      // signup
+      const res = await projectAuth.createUserWithEmailAndPassword(
+        email,
+        password
+      );
+      console.log(res.user);
+
+      /* unsuccessful response */
+      if (!res) {
+        throw new Error('Could not complete signup process');
+      }
+
+      /* successful response */
+      // add display name to user
+      await res.user.updateProfile({ displayName });
+
+      setIsPending(false);
+      setError(null);
+    } catch (err) {
+      console.log(err.message);
+      setError(err.message);
+      setIsPending(false);
+    }
+  };
+
+  return { error, isPending, signup };
+};
