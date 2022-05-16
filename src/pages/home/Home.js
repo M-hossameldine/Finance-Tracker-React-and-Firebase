865f14d037b14294483bc31340e8de087f9f7b52
Mohamed Hossam <m.hossameldine@gmail.com>
2022-05-16 21:38:23 +0200

    feat: fetch transactions in home component

--- src/pages/home/Home.js
+++ src/pages/home/Home.js
@@ -1,5 +1,7 @@
 import React from 'react';
 import { useAuthContext } from '../../hooks/useAuthContext';
+import { useCollection } from '../../hooks/useCollection';
+import TransactionList from './TransactionList';
 
 // styles
 import styles from './Home.module.css';
@@ -9,10 +11,18 @@ import TransactionFrom from './TransactionFrom';
 
 export default function Home() {
   const { user } = useAuthContext();
+  const { documents, error } = useCollection(
+    'transactions',
+    ['uid', '==', user.uid],
+    ['createdAt', 'desc']
+  );
 
   return (
     <div className={styles.container}>
-      <div className={styles.content}></div>
+      <div className={styles.content}>
+        {error && <p>{error}</p>}
+        {documents && <TransactionList transactions={documents} />}
+      </div>
       <div className={styles.sidebar}>
         <TransactionFrom uid={user.uid} />
       </div>
